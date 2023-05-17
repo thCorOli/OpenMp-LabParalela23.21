733c6a72cf41e714e2d151d57420b4f6e5171a2a
Thauan <tholiveira@id.uff.bra>
2023-05-17 20:48:53 -0300

    Dividindo o codigo

--- Naive/primosNaive.c
+++ Naive/primosNaive.c
@@ -18,6 +18,10 @@ int main(int argc, char *argv[]) {
     double t_inicio = omp_get_wtime(); // Pega o tempo em que as threads iniciaram a execução
     omp_set_num_threads(4); // Número de threads a ser usada
 
+	//Funcao para a entrada de dados
+
+
+	//Funcao para validar
 	if (argc < 2) {
         	printf("Valor inválido! Entre com um valor do maior inteiro\n");
        	 	return 0;
@@ -25,12 +29,14 @@ int main(int argc, char *argv[]) {
         	n = strtol(argv[1], (char **) NULL, 10);
        	}
 
+	// Paralelismo
 	#pragma omp parallel for reduction(+:total)
     {
 
     }
     		
 	
+	//Funcao para a saida do programa
 	t_final = omp_get_wtime();
     printf("Quant. de primos entre 1 e n: %d \n", total);
     printf("Tempo de execucao: %1.7f \n", t_final - t_inicio);	 
