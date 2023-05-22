7e7889e7f50535ab0db9cd357b3a1333dda3a92d
Thauan <tholiveira@id.uff.bra>
2023-05-22 11:25:42 -0300

    codigo do Naive feito

--- Naive/primosNaive.c
+++ Naive/primosNaive.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <math.h>
+#define TRUE 1
+#define FALSE 0
+
+void setData(int* setNumThread, long int* setEntrada) {
+	printf("Digite o número de threads: ");
+	scanf("%d",setNumThread);
+	printf("Digite o n para saber a soma de seus primos: ");
+	scanf("%ld",setEntrada);
+	return ;
+}
+
+int validateData (int setNumThread,  long int setEntrada) {
+	if(setNumThread < 1){
+		printf("Digite um número valido de threads\n");
+		return FALSE;
+	}
+	if (setEntrada < 2) {
+        	printf("Valor inválido! Entre com um valor acima de 2.\n");
+       	 	return FALSE;
+	}
+	return TRUE;
+}
 
 int primo (long int n) {
 	int i;
@@ -11,35 +33,36 @@ int primo (long int n) {
 	return 1;
 }
 
-int main(int argc, char *argv[]) {
+void output(int total, double tInicio, double tFinal, long int n) {
+	printf("Quant. de primos entre 1 e %ld: %d \n",n, total);
+    printf("Tempo de execucao: %1.7f \n", tFinal - tInicio);
+}
+
+int main() {
 	double tFinal;
-	int total = 1, numThread = 4;
+	int total = 1, numThread = 0;
 	long int nEntrada;
     double tInicio = omp_get_wtime(); // Pega o tempo em que as threads iniciaram a execução
-    omp_set_num_threads(numThread); // Número de threads a ser usada
 
 	//Funcao para a entrada de dados
-
+	setData(&numThread,&nEntrada);
 
 	//Funcao para validar
-	if (argc < 2) {
-        	printf("Valor inválido! Entre com um valor do maior inteiro\n");
-       	 	return 0;
-    	} else {
-        	nEntrada = strtol(argv[1], (char **) NULL, 10);
-       	}
+	if(validateData(numThread,nEntrada) == FALSE) return 0;
+    
+	// Número de threads a ser usada
+	omp_set_num_threads(numThread); 
 
 	// Paralelismo
 	#pragma omp parallel for reduction(+:total)
-    {
-
-    }
+		for (int i = 3; i <= nEntrada; i += 2) {
+         	if(primo(i) == 1) total++;
+		}	
     		
-	
+	tFinal = omp_get_wtime(); // Pega o tempo em que foi finalizado o programa
+
 	//Funcao para a saida do programa
-	tFinal = omp_get_wtime(); // Pego o tempo em que foi finalizado o programa
-    printf("Quant. de primos entre 1 e n: %d \n", total);
-    printf("Tempo de execucao: %1.7f \n", tFinal - tInicio);	 
+    output(total,tInicio,tFinal,nEntrada);	 
 	
-	return(0);
+	return 0;
 }
